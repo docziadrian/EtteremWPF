d68c75c6ac7ae9ca298319caf74d5499d9a7cd8c
MrErik402 <bujakierikattila@turr.hu>
2025-11-20 12:05:35 +0100

    Értékelés javítás

--- EtteremWPF/Class/Ertekeles.cs
+++ EtteremWPF/Class/Ertekeles.cs
@@ -8,9 +8,11 @@ namespace Szalloda
 {
     internal class Ertekeles
     {
-        private int ID;
-        private int ErtekeltSzallas_ID;
-        private string ErtekeloNeve;
-        private int Pontszam;
+        public string ErtekeloNev { get; set; }
+        public int Pontszam { get; set; }     // 1–5
+        public string Szoveg { get; set; }
+        public DateTime Datum { get; set; }
+        public int SzallasID { get; set; }    // opcionális kapcsolás
+        public string Email { get; set; }
     }
 }
