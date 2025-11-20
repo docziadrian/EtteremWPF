9d6ffe3015fb4bd0b5d51f3d2e2753eea676a8bd
MrErik402 <bujakierikattila@turr.hu>
2025-11-20 11:59:03 +0100

    Osztályok feltöltése

---
+++ EtteremWPF/Class/Ertekeles.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Szalloda
+{
+    internal class Ertekeles
+    {
+        private int ID;
+        private int ErtekeltSzallas_ID;
+        private string ErtekeloNeve;
+        private int Pontszam;
+    }
+}
---
+++ EtteremWPF/Class/Szallas.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Szalloda
+{
+    internal abstract class Szallas
+    {
+        private int ID;
+        private string SzallasNeve;
+        private string Cime;
+
+
+    }
+}
